58fa4c2a0ceba9d73b7a2e21e335be0579099728
Sansossio <juliosansossio@gmail.com>
2020-05-09 22:22:18 +0200

    Example test

--- src/electron/url/base-url/base-url.ts
+++ src/electron/url/base-url/base-url.ts
@@ -1,8 +1,8 @@
-import * as isDev from 'electron-is-dev'
 import { format } from 'url'
 import { join } from 'path'
 
 export function baseUrl () {
+  const isDev = require('electron-is-dev')
   if (isDev) {
     return 'http://localhost:3000'
   }
