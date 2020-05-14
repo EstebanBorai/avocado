2df281ce6f5c6347217eff1198bca86f4ec8aa1f
Sansossio <juliosansossio@gmail.com>
2020-05-14 13:28:41 +0200

    Use electron instead of electron-better-ipc

--- packages/ui/src/service/file.service.ts
+++ packages/ui/src/service/file.service.ts
@@ -1,4 +1,4 @@
-import { ipcRenderer } from 'electron-better-ipc';
+import { ipcRenderer } from 'electron';
 import { IPCEvents, DownloadFileParams } from 'shared';
 
 export interface IFileService {
@@ -9,7 +9,7 @@ class FileService implements IFileService {
   public create({ filename, contents }: DownloadFileParams): void {
     ipcRenderer.send(IPCEvents.CREATE_AND_DOWNLOAD_FILE, {
       filename,
-      contents
+      contents,
     });
   }
 }
